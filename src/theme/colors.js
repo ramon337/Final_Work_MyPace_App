a72728c7274fa23577846385a4396e568c0456c4
ramon_deraes <ramon.de.raes@student.ehb.be>
2026-04-29 19:01:27 +0200

    project setup

---
+++ src/theme/colors.js
@@ -0,0 +1,8 @@
+export const COLORS = {
+  background: '#F1EDED',
+  primaryOrange: '#E7543B',
+  secondaryYellow: '#F6AE30',
+  progressGreen: '#519378',
+  textDark: '#181C2F',
+  white: '#FFFFFF',
+};
